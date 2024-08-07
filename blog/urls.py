a6b0e41138c760adadc46997df72dc8239d04e95
[Arpan Gupta] <arpangupta56@gmail.com>
2024-08-08 00:56:59 +0100

    Enhancing base structure

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
     path('about/', views.about, name = 'blog-about'),
-    path('base',views.base)
+    path('base',views.base, name = 'blog-base')
     ]
